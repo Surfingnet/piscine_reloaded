56a3930977f7a54c93913bf56444d03a9782d0dd
Maxime GHAZARIAN <mghazari@e2r12p16.42.fr>
2016-12-05 11:28:56 +0100

    mac.sh

--- ex04/MAC.sh
+++ ex04/MAC.sh
@@ -6,8 +6,8 @@
 #    By: mghazari <mghazari@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/08 06:39:23 by mghazari          #+#    #+#              #
-#    Updated: 2016/12/05 10:23:08 by mghazari         ###   ########.fr        #
+#    Updated: 2016/12/05 11:26:03 by mghazari         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-ifconfig | grep "ether" | sed 's/\(.*\)ether //g'
+ifconfig | grep "ether" | sed 's/\(.*\)ether //g' | rev | cut -c 2- | rev
